30beda37e92782db135f28b396594d8070b7117b
Ksaw303x <smn.not@outlook.com>
2021-04-04 00:31:03 +0200

    changed on message indentation

--- core/client.py
+++ core/client.py
@@ -19,8 +19,12 @@ class Client(DiscordClient, Observer):
         self.actions = ActionsManager(actions_raw)
         self.routine = RoutineManager(routine_raw, self.logger)
 
+        # the current status of the user
+        self.is_online = False
+
     async def routine_update(self, is_online: bool) -> None:
         if is_online:
+            self.is_online = is_online
             # await self.change_presence(status=Status.do_not_disturb, activity=Game('dunno'))
             self.actions.resume()
         else:
@@ -38,50 +42,51 @@ class Client(DiscordClient, Observer):
 
     async def on_message(self, message):
 
-        if message.author.id in configs.allowed_ids:
+        if message.author.id not in configs.allowed_ids:
+            return
 
-            if message.author.id == configs.trainer.id:
-                for action_raw in self.actions.actions_raw:
+        if message.author.id == configs.trainer.id:
+            for action_raw in self.actions.actions_raw:
 
-                    if message.content.startswith(get_trigger(action_raw)):
-                        for channel_id in get_channels(action_raw):
-                            await self.actions.create(
-                                self.get_channel(channel_id),
-                                action_raw,
-                                self.logger
-                            )
+                if message.content.startswith(get_trigger(action_raw)):
+                    for channel_id in get_channels(action_raw):
+                        await self.actions.create(
+                            self.get_channel(channel_id),
+                            action_raw,
+                            self.logger
+                        )
 
-            if message.author.id == configs.target_id:
-                trainer_mention_admin, trainer_mention = f'<@!{configs.trainer.id}>', f'<@{configs.trainer.id}>'
-                check_string = ' is dropping'
-                if (check_string and (trainer_mention_admin or trainer_mention)) in message.content:
-                    emojis = ['1️⃣', '2️⃣', '3️⃣']
-                    emote = random.choice(emojis)
+        if message.author.id == configs.target_id:
+            trainer_mention_admin, trainer_mention = f'<@!{configs.trainer.id}>', f'<@{configs.trainer.id}>'
+            check_string = ' is dropping'
+            if (check_string and (trainer_mention_admin or trainer_mention)) in message.content:
+                emojis = ['1️⃣', '2️⃣', '3️⃣']
+                emote = random.choice(emojis)
 
-                    action_log = f'reacted with "{emote}"'
-                    await self.logger.log_action(action_log)
+                action_log = f'reacted with "{emote}"'
+                await self.logger.log_action(action_log)
 
-                    await sleep(random.randrange(6, 10))
-                    await message.add_reaction(emote)
+                await sleep(random.randrange(6, 10))
+                await message.add_reaction(emote)
 
-            if message.content.startswith('..log'):
-                self.logger.channel = message.channel
-                await message.channel.send('ok, saved')
+        if message.content.startswith('..log'):
+            self.logger.channel = message.channel
+            await message.channel.send('ok, saved')
 
-            if message.content.startswith('..pause'):
-                self.actions.pause()
-                await message.channel.send('ok, paused')
+        if message.content.startswith('..pause'):
+            self.actions.pause()
+            await message.channel.send('ok, paused')
 
-            if message.content.startswith('..resume'):
-                self.actions.resume()
-                await message.channel.send('ok, resumed')
+        if message.content.startswith('..resume'):
+            self.actions.resume()
+            await message.channel.send('ok, resumed')
 
-            if message.content.startswith('..ignore'):
-                self.routine.follow_trainer_routine = not self.routine.follow_trainer_routine
-                if self.routine.follow_trainer_routine:
-                    await message.channel.send('farmer now based on trainer')
-                else:
-                    await message.channel.send('Persistent farmer')
+        if message.content.startswith('..ignore'):
+            self.routine.follow_trainer_routine = not self.routine.follow_trainer_routine
+            if self.routine.follow_trainer_routine:
+                await message.channel.send('farmer now based on trainer')
+            else:
+                await message.channel.send('Persistent farmer')
 
     async def on_member_update(self, before, after):
 
